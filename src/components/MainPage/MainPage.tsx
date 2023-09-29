c661146f0b9055c9f5141681649c34cc4abbdccb
Alex Cosmas <cosmasalex365@gmail.com>
2023-09-29 13:00:46 +0300

    Loading skeleton

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -26,7 +26,17 @@ function MainPage() {
         <p>Written a couple of articles — some more noteworthy than others.</p>
       </div>
       {loading ? (
-        <div className="centered-div loading">Loading blog posts...</div>
+        <div className="centered-div loading">
+          <div role="status" className="max-w-sm animate-pulse">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
       ) : (
         <BlogGrid articles={articles} />
       )}
